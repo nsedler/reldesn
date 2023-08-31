0b830217c3ab6a7925d55b1c6e679023b4e072ed
Nate Sedler <nsedler1@gmail.com>
2023-08-31 00:47:00 -0400

    added json project list loading

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,40 +1,25 @@
 import React from 'react';
 import PyLogo from './images/python-logo.png';
 
-export default function AboutMe() {
+import Projects from './projects.json';
+
+export default function AboutMe({ project }) {
     return (
         <>
-            <div className='about-parent'>
-                <div className='info-card'>
-                    <div className='info-logo-container'>
-                        <img src={PyLogo} height="128px"/>
-                    </div>
-                    <div className='info-text-container'>
-                        <div id='project-title'>
-                            <a href='/'>Eddie Bot</a>
-                        </div>
-                        <div id='project-description'>
-                            A discord bot made in java and re-written in rust.
-                        </div>
-                        <div id='project-stack'>
-                            Java, JDA (Java Discord API), Herkou
-                        </div>
-                    </div>
+
+            <div className='info-card'>
+                <div className='info-logo-container'>
+                    <img src={PyLogo} height="128px" />
                 </div>
-                <div className='info-card'>
-                    <div className='info-logo-container'>
-                        <img src={PyLogo} height="128px"/>
+                <div className='info-text-container'>
+                    <div id='project-title'>
+                        <a href='/'>{project.title}</a>
                     </div>
-                    <div className='info-text-container'>
-                        <div id='project-title'>
-                            <a href='/'>Eddie Bot</a>
-                        </div>
-                        <div id='project-description'>
-                            A discord bot made in java and re-written in rust.
-                        </div>
-                        <div id='project-stack'>
-                            Java, JDA (Java Discord API), Herkou
-                        </div>
+                    <div id='project-description'>
+                        {project.description}
+                    </div>
+                    <div id='project-stack'>
+                        {project.stack}
                     </div>
                 </div>
             </div>
--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import CHeader from './Header.js';
 import AboutMe from './AboutMe.js';
 import Footer from './Footer';
 
+
+import Projects from './projects.json';
+
 function App() {
   return (
     <div className="App">
-      <CHeader/>
-      <AboutMe/>
-      <Footer/>
+      <CHeader />
+      <div className='about-parent'>
+      { 
+        Projects.map(e => (
+          <AboutMe project={e} />
+        ))
+      }
+      </div>
+      <Footer />
     </div>
   );
 }
