a4baac3c0e1c6e385dc61794894a2bae1bdbab61
Nate Sedler <nsedler1@gmail.com>
2023-09-01 14:22:01 -0400

    projects have a rank att. and is rendered based on rank (#4)

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import Footer from './LinkBar';
 import Projects from './projects.json';
 
 function App() {
+  const sortedProjects = Projects.sort((a, b) => a.rank - b.rank);
   return (
     <div className="App">
     <Footer />
       <CHeader />
       <div className='about-parent'>
       { 
-        Projects.map(e => (
+        sortedProjects.map(e => (
           <AboutMe project={e} />
         ))
       }
