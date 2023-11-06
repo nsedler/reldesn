d2a36176335293a3ee40e8eac90cc24ef5360d98
Nate Sedler <nsedler1@gmail.com>
2023-11-06 13:25:46 -0500

    work on header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import selfie from './images/good_selfie.jpg';
-import cincy from './images/cincy.jpg';
+import github from './images/github-mark-white.png';
+import linkedin from './images/In-White-128@2x.png';
 
 export default function CHeader() {
     return (
         <div className="header-parent">
-            
+            <div className='header-name'>
+                Sedler<br></br>Nate
+            </div>
+            <div className='header-headshot-parent'>
+                <div className='header-headshot'>
+                    <img src={selfie} alt='personal headshot' height="315px"/>
+                </div>
+            </div>
+            <div className='header-img-master-parent'>
+                <div className='header-img-parent'>
+                    <div className='header-img-github'>
+                        <img src={github} height='35px'/>
+                    </div>
+                    <div className='header-img-linkedin'>
+                        <img src={linkedin} height='35px'/>
+                    </div>
+                </div>
+            </div>
+            <div className='header-title-parent'>
+                Web Dev<br />
+                Software Dev<br />
+                Tech Enthusiast<br />
+            </div>
         </div>
     );
 }
